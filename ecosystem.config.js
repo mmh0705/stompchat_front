192b821c5005d9f78587f3ba00b738a42cecfb24
mmh0705 <mmh0705@github.com>
2024-11-20 16:58:33 +0900

    pm2 에코시스템 경로 변경 source로 다시 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
       repo : 'https://github.com/mmh0705/stompChat_front.git',
       path : '/home/siin',
       'pre-deploy-local': '',
-      'post-deploy' : 'cd /home/siin/source2 && source2 ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'cd /home/siin/source && source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options' : 'ForwardAgent=yes'
     }
