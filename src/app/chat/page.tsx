1bd8bab7ab461e356f8fae76beb36aebe4bd0e2f
mmh0705 <mmh0705@github.com>
2025-01-14 15:52:12 +0900

    화면에 채팅 내용이 나오도록 수정해보았다.

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,14 +1,41 @@
 'use client';
 
+// let messageData = {
+//     user: "익명",
+//     message: "메시지",
+
+//     isOnOpen: false,
+//     isOnClose: false,
+//     userCount: 0,
+// }
+
+
 import { useState, useEffect, useRef } from "react";
 import * as StompJs from "@stomp/stompjs";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
 
 export default function Chat() {   
+    type messageJSON = {
+        id : number,
+        user : string,
+        message : string,
+        userCounts : number
+    }
+    //useRef
+    const websocket = useRef<WebSocket | null>(null);
+    const userName = useRef<string>("익명");
+    
+    //useState
     const [client] = useState<StompJs.Client | null>(null);
     const [message, setMessage] = useState("");
     const [messageList, setMessageList] = useState<string[]>(['']);
+    const [connectedClientsCount , setConnectedClientsCount] = useState(0);
+    //채팅들
+    const [chatList, setChatList] = useState<string[]>(["welcome"]);
 
-    const websocket = useRef<WebSocket | null>(null);
     const localSocketURL = "ws://localhost:8080/user/websocket";
     const remoteSocketURL = "ws://121.162.75.86:8080/user/websocket";
     //const [websocket, setWebsocket] = useState<WebSocket>(new WebSocket("ws://localhost:8080/user/websocket"));
@@ -17,36 +44,6 @@ export default function Chat() {
      * 연결
      */
     const connect = () =>{
-        // STOMP example
-        // //소켓 연결
-        // try{
-        //     const stompClient = new StompJs.Client({
-        //         //brokerURL: "ws://121.162.75.86:8080/gs-guide-websocket",
-        //         brokerURL: "ws://localhost:8080/gs-guide-websocket",
-        //         reconnectDelay: 5000, // 자동 재 연결
-        //         heartbeatIncoming: 4000,
-        //         heartbeatOutgoing: 4000,
-        //     });
-            
-        //     setClient(stompClient);
-            
-        //     stompClient.onConnect = (frame) =>{
-        //         console.log('Connected: ' + frame);
-        //         stompClient.subscribe('/topic/greetings', (greeting) =>{
-        //             showGreeting(JSON.parse(greeting.body).content);
-        //         });
-        //     }
-
-        //     stompClient.activate();
-
-        // }
-        // catch(err){
-        //     console.error(err);
-        // }
-        
-        // Vanilla WebSocket
-        // let temp = new WebSocket("ws://localhost:8080/user/websocket");
-        // setWebsocket(temp);
         websocket.current = new WebSocket(remoteSocketURL);
         websocket.current.onopen = () => {
             console.log('웹소켓 열림');
@@ -61,41 +58,31 @@ export default function Chat() {
         }
 
         websocket.current.onmessage = (event) => {
-            console.log('메시지 : ' + event.data);
-            //접속 캐치
-                
-            //일반 메시지
+            let json:messageJSON = JSON.parse(event.data);
+            
+            //접속자 갱신
+            setConnectedClientsCount(json.userCounts);
+
+            //메시지 추가
+            console.log(chatList);
+            console.log(...chatList);
+            console.log(json.message);
+            let list = [...chatList, json.message];
+            console.log(list);
+            setChatList(chatList => [...chatList, json.message]);
+            console.log(...chatList);
         };
     
-    }
-
-    /**
-     * 연결 끊기
-     */
-    const disconnect = () =>{
-        if(client === null){
-            return;
-        } 
-        client.deactivate();
     }
 
     /**
      * 메시지 보내기
      */
     const sendChat = ()=>{
-        // if(client === null){
-        //     return;
-        // }
-        // console.log("sendChat");
-        // client.publish({
-        //     destination: "/app/hello",
-        //     body: JSON.stringify({name: name})
-        // });
+      
         let messageData = {
             user: "익명",
             message: message,
-            isOnOpen: false,
-            isOnClose: false
         }
         websocket.current?.send(JSON.stringify(messageData));
     }
@@ -125,8 +112,8 @@ export default function Chat() {
             {/* 바깥 */}
             <div className="flex flex-row items-center h-52"> 
                 {/* 왼쪽 */}
-                <div className="bg-white min-w-60 h-full">
-                    접속자 목록    
+                <div className="bg-white min-w-60 h-full text-black">
+                    현재 접속자 숫자 : {connectedClientsCount}    
                 </div>
                 
                 {/* 오른쪽 */}
@@ -144,10 +131,23 @@ export default function Chat() {
                     {/* 채팅 입력 */}
                     <div>
                         <div>
-                            <button className="bg-slate-500" onClick={sendChat}>전송</button>
+                            <Button className="text-black" color="primary" variant="contained" onClick={sendChat}>
+                                전송
+                            </Button>
+
+                            <input value={message} onChange={onSetMessage}></input>
                         </div>
+                        
                         <div>
-                            <input type="text" value={message} onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setMessage(event.target.value)}}></input>
+                            <ul>
+                               {
+                                chatList.map((message) => {
+                                    return(
+                                        <li key={message}>{message}</li>
+                                    )
+                                })
+                               }
+                            </ul>
                         </div>
                     </div>
                     
@@ -157,3 +157,58 @@ export default function Chat() {
         </div>
     );
 }   
+
+
+//정크 존
+
+// STOMP example
+        // //소켓 연결
+        // try{
+        //     const stompClient = new StompJs.Client({
+        //         //brokerURL: "ws://121.162.75.86:8080/gs-guide-websocket",
+        //         brokerURL: "ws://localhost:8080/gs-guide-websocket",
+        //         reconnectDelay: 5000, // 자동 재 연결
+        //         heartbeatIncoming: 4000,
+        //         heartbeatOutgoing: 4000,
+        //     });
+            
+        //     setClient(stompClient);
+            
+        //     stompClient.onConnect = (frame) =>{
+        //         console.log('Connected: ' + frame);
+        //         stompClient.subscribe('/topic/greetings', (greeting) =>{
+        //             showGreeting(JSON.parse(greeting.body).content);
+        //         });
+        //     }
+
+        //     stompClient.activate();
+
+        // }
+        // catch(err){
+        //     console.error(err);
+        // }
+        
+        // Vanilla WebSocket
+        // let temp = new WebSocket("ws://localhost:8080/user/websocket");
+        // setWebsocket(temp);
+
+
+
+          // if(client === null){
+        //     return;
+        // }
+        // console.log("sendChat");
+        // client.publish({
+        //     destination: "/app/hello",
+        //     body: JSON.stringify({name: name})
+        // });
+
+    /**
+     * 연결 끊기
+     */
+    // const disconnect = () =>{
+    //     if(client === null){
+    //         return;
+    //     } 
+    //     client.deactivate();
+    // }
